ab5a947583bdbd6a44bde5c71ebe0ebaeba478aa
arpita199812 <arpitasingh199812@gmail.com>
2024-04-10 17:49:17 +0530

    new commit

---
+++ src/main/java/arp/App.java
@@ -0,0 +1,18 @@
+package arp;
+
+import java.util.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        public static void main(String[] args) {
+            Generator generator = new Generator(keyboard);
+            generator.mainLoop();
+            keyboard.close();
+        }
+    }
